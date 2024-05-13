0c02a05d5f68f0ca6ee5373dbe86e5e6eecd39fe
Brendon <bluicien@yahoo.com>
2024-05-13 19:01:25 +0900

    Added verification for user registration endpoint.

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const userRouter = express.Router();
 const { db } = require('../db/index')
+const { userExists } = require('../db/verification');
 
-userRouter.get('/:userId', async (req, res) => {
+userRouter.get('/:userId', (req, res) => {
     const id = parseInt(req.params.userId);
 
-    await db.query('SELECT * FROM users WHERE id = $1', [id], (error, result) => {
+    db.query('SELECT * FROM users WHERE id = $1', [id], (error, result) => {
         if (error) {
             throw error;
         }
@@ -13,27 +14,28 @@ userRouter.get('/:userId', async (req, res) => {
     });
 });
 
-userRouter.post('/register', async (req, res) => {
-    const { username, password, first_name, last_name,email } = req.body;
+userRouter.post('/register', userExists, (req, res) => {
+    const { username, password, first_name, last_name, email } = req.body;
     const values = [username, password, first_name, last_name, email];
     const text = "INSERT INTO users (username, password, first_name, last_name, email) VALUES ($1, $2, $3, $4, $5) RETURNING *";
 
-    await db.query(text, values, (error, result) => {
+    db.query(text, values, (error, results) => {
         if (error) {
-            throw error;
+            res.status(400).send("Bad Request");
         }
-        res.status(201).json(result.rows[0]);
-    })
+        res.status(201).json(results.rows[0]);
+    });
+    
 })
 
 
-userRouter.put('/:userId', async (req, res) => {
+userRouter.put('/:userId', (req, res) => {
     const id = parseInt(req.params.userId);
     const { password, first_name, last_name, email, birth_date } = req.body;
     const text = 'UPDATE users SET password = $1, first_name = $2, last_name = $3, email = $4, birth_date = $5 WHERE id = $6';
     const parameters = [password, first_name, last_name, email, birth_date, id];
     
-    await db.query(text, parameters, (error, results) => {
+    db.query(text, parameters, (error, results) => {
         if (error) {
             throw error;
         } 
@@ -41,11 +43,11 @@ userRouter.put('/:userId', async (req, res) => {
     })    
 });
 
-userRouter.delete('/:userId', async (req, res) => {
+userRouter.delete('/:userId', (req, res) => {
     const id = parseInt(req.params.userId);
     const text = 'DELETE FROM users WHERE id = $1';
 
-    await db.query(text, [id], (error, results) => {
+    db.query(text, [id], (error, results) => {
         if (error) {
             res.status(400).send(error);
         }
