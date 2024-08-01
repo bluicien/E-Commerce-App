8fd4b10640ae510531dd0c0f37eda65f5387892a
Brendon <bluicien@yahoo.com>
2024-08-01 20:52:05 +0900

    deleted next api folder

--- view/src/app/api/products/route.js
+++
@@ -1,8 +0,0 @@
-
-export async function GET() {
-    console.log("GET products...")
-    const BASE_URL = "http://localhost:3000";
-    const response = await fetch(`${BASE_URL}/products`)
-    const products = await response.json();
-    return Response.json(products);
-}
