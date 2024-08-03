d4fbcbaa21dcb798469ed8d77d59c5d13cac7ffb
Brendon <bluicien@yahoo.com>
2024-08-03 17:31:09 +0900

    changes secure to true

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.use(
         cookie: {
             maxAge: 1000 * 60 * 60,
             sameSite: 'lax',
-            secure: false, // True for production
+            secure: true, // True for production
             httpOnly: true
         },
         store: redisStore,
